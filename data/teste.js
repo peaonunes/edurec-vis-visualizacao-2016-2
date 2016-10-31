210f2d1e40cca58ed442b762fbac854ed6fc45e4
Daniel Oliveira <dvalbrand@gmail.com>
2016-10-31 10:22:57 -0300

    Finished school data aggregation and filtering

--- data/teste.js
+++ data/teste.js
@@ -1,1 +1,7 @@
-console.log(__dirname)
+const fs = require('fs');
+
+const aggregateAndFilter = require('./postProcessing/aggregateAndFilterSchoolData');
+
+const schoolsData = JSON.parse(fs.readFileSync('dataFiles/escolas2015.json'));
+
+console.log(aggregateAndFilter(schoolsData.entries));
