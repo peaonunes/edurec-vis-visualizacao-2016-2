2973a621a654bec5ba32bb0959f94d6ce768f7c7
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-11-01 21:26:23 -0300

    Map render.

--- src/app/entry.js
+++ src/app/entry.js
@@ -2,7 +2,41 @@ const d3 = require('d3');
 const leaflet = require('leaflet');
 
 document.addEventListener('DOMContentLoaded', () => {
-  d3.select('#content')
-    .append('h1')
-    .text('Coming soon...');
+    d3.select('#content')
+        .append('h1')
+        .text('Coming soon...');
+
+    renderMap();
 });
+
+function renderMap() {
+    setupMap();
+
+    var schools = [
+
+    ];
+
+    renderPoints(schools);
+}
+
+function setupMap(){
+    var content = d3.select("#content");
+
+    content.append("div")
+        .attr("id", "map")
+        .attr("style", "height: 500px !important; width: 900px !important;");
+
+    var map = leaflet.map("map").setView([-8.079, -34.920], 12);
+
+    leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(map);
+}
+
+function renderPoints(schools) {
+
+}
--- src/app/ui/map.js
+++ src/app/ui/map.js
@@ -1,32 +1,19 @@
-/*var map;
+var map;
 
-function init(){
-    map = L.map('map').setView([8.047, 34.877], 13);
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-}
-
-init();
-*/
-
-/* var map;
-
-function init(){
+function renderMap() {
     var content = d3.select("#content");
 
     content.append("div")
         .attr("id", "map")
-        .attr("height", "400px;")
-        .attr("width", "800px;");
+        .attr("style", "height: 500px !important; width: 900px !important;");
 
-    map = leaflet.map('map').setView([8.047, 34.877], 13);
+    map = leaflet.map('map').setView([-8.079, -34.920], 12);
 
-    leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
     }).addTo(map);
 }
-
-init();
-*/
