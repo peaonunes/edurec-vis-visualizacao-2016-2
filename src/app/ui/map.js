381f9e49f2046a8127cea9e9c7f20e07ac198f0a
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2016-11-03 17:13:32 -0300

    Schools details first version.

--- src/app/ui/map.js
+++ src/app/ui/map.js
@@ -10,16 +10,25 @@ export function renderMap() {
     var schools = [
         {
             "nome": "ESCOLA MUNICIPAL IRMA TEREZINHA BATISTA - ANEXO I",
+            "rank": 80,
+            "address": "R. dos Craveiros, 273 - Campina do Barreto, Recife - PE, 52121-370, Brazil",
+            "email": "EM.TEREZINHABATISTA@EDUCARECIFE.COM.BR",
             "lat": -8.014138299999999,
             "lng": -34.8813573
         },
         {
             "nome": "ESCOLA MUNICIPAL ENGENHEIRO UMBERTO GONDIM",
             "lat": -8.0847,
+            "rank": 60,
+            "email": "ESCUMBERTO@YAHOO.COM.BR",
+            "address": "R. das Oficinas, 13 - Pina, Recife - PE, 51010-680, Brazil",
             "lng": -34.884
         },
         {
             "nome": "ESCOLA MUNICIPAL POETA PAULO BANDEIRA DA CRUZ",
+            "email": "EM.POETAPBANDEIRA@HOTMAIL.COM",
+            "address": "R. Panelas, 28 - COHAB, Recife - PE, Brazil",
+            "rank":75,
             "lat": -8.121,
             "lng": -34.955
         }
@@ -62,5 +71,23 @@ function renderMarkers(schools) {
 }
 
 function moreDetails(school){
-    return "<b>"+school["nome"]+"</b><br/>";
+    var st = detailsLayout.replace("#rank", school["rank"]);
+    st = st.replace("#name", school["nome"]);
+    st = st.replace("#address", school["address"]);
+    st = st.replace("#email", school["email"]);
+    return st;
 }
+
+/*
+<div>
+    <div>
+        <h5>#rank</h5>
+    </div>
+    <div>
+        <h5>#name</h5></b><br/>
+        <p>#address</p></b><br/>
+        <p>#email</p></b><br/>
+    </div>
+</div>
+*/
+var detailsLayout = "<div><div><h2>#rank%</h2></div><div><h3>#name</h3><p>#address</p><p>#email</p></div></div>";
