5599c5fb97edc9c02768df9e419be7beb211c15b
cb16 <cscbb@cin.ufpe.br>
2016-11-07 19:15:46 -0300

    adding calculate school grade

---
+++ src/app/ui/metrics.js
@@ -0,0 +1,50 @@
+var schools = [
+    {
+        "nome": "ESCOLA MUNICIPAL IRMA TEREZINHA BATISTA - ANEXO I",
+        "rank": 80,
+        "address": "R. dos Craveiros, 273 - Campina do Barreto, Recife - PE, 52121-370, Brazil",
+        "email": "EM.TEREZINHABATISTA@EDUCARECIFE.COM.BR",
+        "lat": -8.014138299999999,
+        "lng": -34.8813573,
+        "alunos": [
+          {
+            "nome": "camila",
+            "SITU": "AP"
+          },
+          {
+            "nome": "peao",
+            "SITU": "RN"
+          },
+          {
+            "nome": "jesus",
+            "SITU": "MO"
+          }
+        ]
+   }
+];
+
+export function calculateSchoolGrade() {
+  for(var i = 0; i < schools.length; i++) {
+    var school = schools[i];
+    var students = school.alunos;
+    var ap = 0;
+    var total = 0;
+    for(var j = 0; j < students.length; j++) {
+      var student = students[j];
+      if(student.SITU == student_situations.AP)
+        ap++;
+      if(student_situations.hasOwnProperty(student.SITU))
+        total ++;
+    }
+    schools["grade"] = (ap / total) * 100;
+  }
+  return schools;
+}
+
+var student_situations = {
+  "AP": "AP",
+  "RN": "RN",
+  "D": "D",
+  "RT": "RT",
+  "RF": "RF"
+}
