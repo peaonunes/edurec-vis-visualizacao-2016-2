6f63a4e388733a6288d67c0754f8d93eccabeb01
cb16 <cscbb@cin.ufpe.br>
2016-11-18 10:27:20 -0300

    piechart first steps

--- src/app/ui/schoolDetails.js
+++ src/app/ui/schoolDetails.js
@@ -53,7 +53,7 @@ function renderStudentsDetails(students) {
   var height = 200;
 
   var g = schoolDetails.append("svg")
-    .attr("id", "studentDetailsHistogram")
+    .attr("id", "studentDetailsPiechart")
     .attr("width", width)
     .attr("height", height)
     .append("g")
@@ -68,4 +68,39 @@ function renderStudentsDetails(students) {
   .style("stroke-width", 1)
   .style("fill", "none");
 
+  var studentTypeQt = {};
+
+  for(var i = 0; i < students.size; i++) {
+    var type = students.get(i).get("situacao");
+
+    if(studentTypeQt.hasOwnProperty(type)) {
+      var value = studentTypeQt[type];
+      studentTypeQt[type] = value + 1;
+    } else {
+      studentTypeQt[type] = 1;
+    }
+  }
+
+  var data = [];
+
+  Object.keys(studentTypeQt).forEach((key) => {
+    data.push(studentTypeQt[key]);
+  });
+  
+  //piechat
+  var colorScale = ["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00"];
+
+  var arc = d3.arc().innerRadius(0).outerRadius(50);
+
+  var pie = d3.pie().value(function(d) {return d;});
+
+  var selection = d3.select("#studentDetailsPiechart").select("g").selectAll("path").data(pie(data));
+
+  selection.enter()
+  .append("path")
+  .attr("d", arc)
+  .attr("fill", function(d,i) {
+    return colorScale[i];
+  })
+  .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
 }
